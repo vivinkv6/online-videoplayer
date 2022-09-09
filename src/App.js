b5b156c8b547c52527c31638a898d3d15a64fc6f
vivinkv6 <vivinkv0@gmail.com>
2022-09-09 07:49:12 +0530

    add new image in navbar

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
   const [url, setUrl] = useState('');
   const [error, setError] = useState(false);
   const [visible, setVisible] = useState(false);
-  const [check,setCheck]=useState(true);
+  const [check, setCheck] = useState(true);
 
 
   const submition = (e) => {
     e.preventDefault();
-    if (!isNaN(url)||url==='') {
+    if (!isNaN(url) || url === '') {
       setError(true);
       setVisible(false);
     }
@@ -29,11 +29,11 @@ function App() {
 
 
 
-  const correct=()=>{
+  const correct = () => {
     setVisible(true);
     setCheck(true);
   }
-  const wrong=()=>{
+  const wrong = () => {
     setVisible(false);
     setCheck(false);
   }
@@ -42,8 +42,16 @@ function App() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">
-            <h2 style={{fontWeight:'700'}}>VideoPlayer</h2>
+          <Navbar.Brand>
+            <img
+              alt=""
+              src="https://www.freeiconspng.com/thumbs/video-icon/video-player-icon-15.png"
+              width="50"
+              height="50"
+              className="d-inline-block align-top "
+              style={{ borderRadius: '50px' }}
+            />
+            <h2 style={{ fontWeight: '700', display: 'inline', position: 'fixed', top: '20px', marginLeft: '10px' }}>VideoPlayer</h2>
           </Navbar.Brand>
         </Container>
       </Navbar>
@@ -51,11 +59,11 @@ function App() {
       <div className='input'>
         <div style={{ backgroundColor: '#f2f2f0', padding: '20px 25px', boxShadow: '0px 0px 5px black' }}>
           <input className='input' value={url} onChange={(e) => setUrl(e.target.value)} style={{ height: '35px', borderRadius: '6px', border: 'none', boxShadow: '0px 0px 5px black' }} type="text" placeholder='Enter URL' />
-       
-          <button style={{display:'inline'}} className='btn btn-primary mt-3' onClick={submition}>Submit</button>
-          <button className='btn btn-primary ms-5 mt-3' onClick={()=>setUrl('')}>Clear</button>
-          
-      
+
+          <button style={{ display: 'inline' }} className='btn btn-primary mt-3' onClick={submition}>Submit</button>
+          <button className='btn btn-primary ms-5 mt-3' onClick={() => setUrl('')}>Clear</button>
+
+
           {error &&
             <Alert variant='danger' className='mt-3'>
               Invalid Input
@@ -63,21 +71,21 @@ function App() {
           }
         </div>
       </div>
-     
 
-     
-        <div className="videoplayer">
-        {visible  || check ?
-          <ReactPlayer controls style={{ marginLeft: '10px', marginRight: '10px' }} height={250} width={500} url={url}  onReady={correct} onError={wrong}/>
-  :
-  <Alert>No Such Video url</Alert>
+
+
+      <div className="videoplayer">
+        {visible || check ?
+          <ReactPlayer controls style={{ marginLeft: '10px', marginRight: '10px' }} height={250} width={500} url={url} onReady={correct} onError={wrong} />
+          :
+          <Alert>No Such Video url</Alert>
         }
-        
-       
-      
-      
-          </div>
-      
+
+
+
+
+      </div>
+
     </>
   );
 }
